07e42db9bced79325fec3a7895017ce672c53500
Gal Barequet <gali.barequet@gmail.com>
2021-05-21 16:32:23 +0300

    Changes for successful execution on Colab

--- logger.py
+++ logger.py
@@ -7,14 +7,18 @@ import tensorflow as tf
 class Logger(object):
 
     def __init__(self, log_dir):
-        self.writer = tf.summary.FileWriter(log_dir)
+        return
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        return
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+
 
     def image_summary(self, tag, image, step):
+        return
         s = BytesIO()
         scipy.misc.toimage(image).save(s, format="png")
 
@@ -26,11 +30,13 @@ class Logger(object):
         )
 
         # Create and write Summary
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, image=img_sum)])
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+
 
     def image_list_summary(self, tag, images, step):
+        return
         if len(images) == 0:
             return
         img_summaries = []
@@ -39,18 +45,19 @@ class Logger(object):
             scipy.misc.toimage(img).save(s, format="png")
 
             # Create an Image object
-            img_sum = tf.Summary.Image(
-                encoded_image_string=s.getvalue(),
-                height=img.shape[0],
-                width=img.shape[1],
-            )
+            # img_sum = tf.Summary.Image(
+            #     encoded_image_string=s.getvalue(),
+            #     height=img.shape[0],
+            #     width=img.shape[1],
+            # )
 
             # Create a Summary value
-            img_summaries.append(
-                tf.Summary.Value(tag="{}/{}".format(tag, i), image=img_sum)
-            )
+            # img_summaries.append(
+            #     tf.Summary.Value(tag="{}/{}".format(tag, i), image=img_sum)
+            # )
 
         # Create and write Summary
-        summary = tf.Summary(value=img_summaries)
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+
--- utils.py
+++ utils.py
@@ -8,7 +8,12 @@ def dsc(y_pred, y_true, lcc=True):
     if lcc and np.any(y_pred):
         y_pred = np.round(y_pred).astype(int)
         y_true = np.round(y_true).astype(int)
-        y_pred = largest_connected_component(y_pred)
+        print('y_pred: ' + str(y_pred))
+        print('type(y_pred):' + str(type(y_pred)))
+        try:
+            y_pred = largest_connected_component(y_pred)
+        except ValueError:
+            np.savetxt('./logs/data.csv', y_pred, delimiter=',')
     return np.sum(y_pred[y_true == 1]) * 2.0 / (np.sum(y_pred) + np.sum(y_true))
 
 

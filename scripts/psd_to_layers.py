4bf89b89c66fe8d6c7abdefca824fe6153a9ff24
Ikalev111 <ikalev111@gmail.com>
2021-05-30 19:14:34 +0300

    added support for fat layer in psd_to_layers

--- scripts/psd_to_layers.py
+++ scripts/psd_to_layers.py
@@ -22,8 +22,11 @@ def psd_to_layers(psd_dir, save_dir):
             psd_file = PSDImage.open(os.path.join(psd_dir, filename))
             layer_raw_image = psd_file[0]
             layer_no_background = psd_file[1]
-            layer_bones = psd_file[2]
-            # layer_fat = psd_file[3] #Doesn't exist currently
+            for layer in psd_file[2:]: # for some reason the layers are switching order from photo to photo
+                if layer.mask is None:
+                    layer_bones = layer
+                else:
+                    layer_fat = layer
 
             raw_image = layer_raw_image.topil()
             raw_image_arr = layer_raw_image.numpy()
@@ -48,6 +51,15 @@ def psd_to_layers(psd_dir, save_dir):
             bones_arr = np.where(bones_arr < 0.1, np.zeros(bones_arr.shape), np.ones(bones_arr.shape))
             bones = Image.fromarray((bones_arr * 255).astype(np.uint8))
             bones.save(os.path.join(bones_dir_path, filename[:-4] + "_bones.png"))
+
+            #creates a mask for the fat (1 - bone, 0 - not bone)
+            fat_arr = np.zeros((raw_image_arr.shape[0], raw_image_arr.shape[1]))
+            bb_fat = layer_fat.bbox
+            fat_arr[bb_fat[1]: bb_fat[3] , bb_fat[0] : bb_fat[2]] = np.array(layer_fat.mask.topil())
+            fat = Image.fromarray(fat_arr)
+            fat = fat.convert('L')
+            fat.save(os.path.join(fat_dir_path, filename[:-4] + "_fat.png"))
+
     return
 
 

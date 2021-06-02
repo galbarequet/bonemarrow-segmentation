ac2a7e93af6cd5a5253d6c24d167e1e44f0b15de
Sagi Tauber <sagi.gw2@gmail.com>
2021-06-02 14:08:03 +0300

    can now work with bone marrow dataset

--- dataset.py
+++ dataset.py
@@ -9,11 +9,48 @@ from torch.utils.data import Dataset
 from utils import crop_sample, pad_sample, resize_sample, normalize_volume
 
 
-class BrainSegmentationDataset(Dataset):
+class BoneMarrowDataset(Dataset):
     """Brain MRI dataset for FLAIR abnormality segmentation"""
 
     in_channels = 3
-    out_channels = 1
+    out_channels = 2
+
+
+    def create_mask(self, bone_layer, fat_layer):
+        bone_layer = (bone_layer/255).astype('uint8')
+        fat_layer = (fat_layer/255).astype('uint8')
+        #background_layer = np.zeros_like(bone_layer, dtype='uint8')
+
+        #background_layer = background_layer + bone_layer + fat_layer
+        #background_layer[background_layer != 0] = 1
+        #background_layer = 1 - background_layer
+        #background_layer = background_layer.astype('uint8')
+
+        mask = np.stack([bone_layer, fat_layer])
+        mask = mask.transpose(1, 2, 0)
+        return mask
+
+
+    def load_dataset(self, image_dir):
+        data = []
+        masks = []
+        raw_image_dir = 'raw_image'
+        bone_layer_dir = 'bones'
+        fat_layer_dir = 'fat'
+        raw_image_full_path = os.path.join(image_dir, raw_image_dir)
+        for i, filename in enumerate(os.listdir(raw_image_full_path)):
+            raw_img = np.array(imread(os.path.join(raw_image_full_path, filename)))
+
+            bone_layer = np.array(imread(os.path.join(os.path.join(image_dir, bone_layer_dir), filename), as_gray=True))
+
+            fat_layer = np.array(imread(os.path.join(os.path.join(image_dir, fat_layer_dir), filename), as_gray=True))
+
+            data.append(raw_img)
+            masks.append(self.create_mask(bone_layer, fat_layer))
+
+        return data, masks
+
+
 
     def __init__(
         self,
@@ -22,115 +59,58 @@ class BrainSegmentationDataset(Dataset):
         image_size=256,
         subset="train",
         random_sampling=True,
-        validation_cases=10,
+        validation_cases=4,
         seed=42,
     ):
         assert subset in ["all", "train", "validation"]
 
         # read images
-        volumes = {}
-        masks = {}
-        print("reading {} images...".format(subset))
-        for (dirpath, dirnames, filenames) in os.walk(images_dir):
-            image_slices = []
-            mask_slices = []
-            for filename in sorted(
-                filter(lambda f: ".tif" in f, filenames),
-                key=lambda x: int(x.split(".")[-2].split("_")[4]),
-            ):
-                filepath = os.path.join(dirpath, filename)
-                if "mask" in filename:
-                    mask_slices.append(imread(filepath, as_gray=True))
-                else:
-                    image_slices.append(imread(filepath))
-            if len(image_slices) > 0:
-                patient_id = dirpath.split("/")[-1]
-                volumes[patient_id] = np.array(image_slices[1:-1])
-                masks[patient_id] = np.array(mask_slices[1:-1])
+        self.images, self.masks = self.load_dataset(images_dir)
 
-        self.patients = sorted(volumes)
-
-        # Note: by default our dataset is large, but in case we we have a small dataset then we must look at number of patients
-        validation_cases = min(validation_cases + 1, len(self.patients)) - 1
 
         # select cases to subset
+        #TODO: This random shit is way to wack please fix this
         if not subset == "all":
             random.seed(seed)
-            validation_patients = random.sample(self.patients, k=validation_cases)
+            indices = [i for i in range(len(self.masks))]
+            validation_indices = random.sample(indices, k=validation_cases)
+            validation_images = [self.images[i] for i in validation_indices]
+            validation_masks = [self.masks[i] for i in validation_indices]
             if subset == "validation":
-                self.patients = validation_patients
+                self.images = validation_images
+                self.masks = validation_masks
             else:
-                self.patients = sorted(
-                    list(set(self.patients).difference(validation_patients))
+                train_indices = sorted(
+                    list(set(indices).difference(validation_indices))
                 )
+                self.images = [self.images[i] for i in train_indices]
+                self.masks = [self.masks[i] for i in train_indices]
 
-        print("preprocessing {} volumes...".format(subset))
-        # create list of tuples (volume, mask)
-        self.volumes = [(volumes[k], masks[k]) for k in self.patients]
-
-        print("cropping {} volumes...".format(subset))
-        # crop to smallest enclosing volume
-        self.volumes = [crop_sample(v) for v in self.volumes]
-
-        print("padding {} volumes...".format(subset))
-        # pad to square
-        self.volumes = [pad_sample(v) for v in self.volumes]
-
-        print("resizing {} volumes...".format(subset))
-        # resize
-        self.volumes = [resize_sample(v, size=image_size) for v in self.volumes]
-
-        print("normalizing {} volumes...".format(subset))
-        # normalize channel-wise
-        self.volumes = [(normalize_volume(v), m) for v, m in self.volumes]
-
-        # probabilities for sampling slices based on masks
-        self.slice_weights = [m.sum(axis=-1).sum(axis=-1) for v, m in self.volumes]
-        self.slice_weights = [
-            (s + (s.sum() * 0.1 / len(s))) / (s.sum() * 1.1) for s in self.slice_weights
-        ]
-
-        # add channel dimension to masks
-        self.volumes = [(v, m[..., np.newaxis]) for (v, m) in self.volumes]
-
-        print("done creating {} dataset".format(subset))
-
-        # create global index for patient and slice (idx -> (p_idx, s_idx))
-        num_slices = [v.shape[0] for v, m in self.volumes]
-        self.patient_slice_index = list(
-            zip(
-                sum([[i] * num_slices[i] for i in range(len(num_slices))], []),
-                sum([list(range(x)) for x in num_slices], []),
-            )
-        )
 
         self.random_sampling = random_sampling
 
         self.transform = transform
 
     def __len__(self):
-        return len(self.patient_slice_index)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        patient = self.patient_slice_index[idx][0]
-        slice_n = self.patient_slice_index[idx][1]
+        image = self.images[idx]
+        mask = self.masks[idx]
 
         if self.random_sampling:
-            patient = np.random.randint(len(self.volumes))
-            slice_n = np.random.choice(
-                range(self.volumes[patient][0].shape[0]), p=self.slice_weights[patient]
-            )
+            idx = np.random.randint(len(self.images))
+            image = self.images[idx]
+            mask = self.masks[idx]
 
-        v, m = self.volumes[patient]
-        image = v[slice_n]
-        mask = m[slice_n]
 
         if self.transform is not None:
-            image, mask = self.transform((image, mask))
+            image, mask = self.transform([image, mask])
+            #mask_tensor = self.transform(mask_tensor)
 
-        # fix dimensions (C, H, W)
-        image = image.transpose(2, 0, 1)
-        mask = mask.transpose(2, 0, 1)
+            # fix dimensions (C, H, W)
+            image = image.transpose(2, 0, 1)
+            mask = mask.transpose(2, 0, 1)
 
         image_tensor = torch.from_numpy(image.astype(np.float32))
         mask_tensor = torch.from_numpy(mask.astype(np.float32))

3a00e4f1911f81a1c60590266d209f0853ab81e1
Sagi Tauber <sagi.gw2@gmail.com>
2021-06-02 14:07:04 +0300

    changed dsc to work with muli layer

--- utils.py
+++ utils.py
@@ -4,17 +4,10 @@ from skimage.exposure import rescale_intensity
 from skimage.transform import resize
 
 
-def dsc(y_pred, y_true, lcc=True):
-    if lcc and np.any(y_pred):
-        y_pred = np.round(y_pred).astype(int)
-        y_true = np.round(y_true).astype(int)
-        print('y_pred: ' + str(y_pred))
-        print('type(y_pred):' + str(type(y_pred)))
-        try:
-            y_pred = largest_connected_component(y_pred)
-        except ValueError:
-            np.savetxt('./logs/data.csv', y_pred, delimiter=',')
-    return np.sum(y_pred[y_true == 1]) * 2.0 / (np.sum(y_pred) + np.sum(y_true))
+def dsc(y_pred, y_true):
+    y_pred = np.round(y_pred).astype(int)
+    y_true = np.round(y_true).astype(int)
+    return np.sum(y_pred == y_true) / y_true.size
 
 
 def crop_sample(x):

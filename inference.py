relevent_masks - changed dir name to fit to the psd script sliding_window - fixed normalization inference - returned argmax baseline - added optional prints

inference.py
@@ -47,19 +47,22 @@ def main(args):
             if args.baseline:
                 x = x.detach().numpy()
                 normal_image = x[0, ...].transpose(1, 2, 0).astype(np.uint8)
-                y_pred_np = np.array([segment_image(normal_image)])
+                y_pred_np = segment_image(normal_image)
                 y_true_np = y_true.detach().numpy()
                 x_np = x
             else:
                 x, y_true = x.to(device), y_true.to(device)
                 y_pred = sliding_window_predictor.predict_image(x)
-                y_pred_np = y_pred.detach().cpu().numpy()
+                y_pred_np = y_pred.detach().cpu().numpy()[0, ...]
+                y_pred_np = np.argmax(y_pred_np, axis=0)
                 x_np = x.detach().cpu().numpy()
                 y_true_np = y_true.detach().cpu().numpy()
 
-            true_list.extend([y_true_np[s] for s in range(y_true_np.shape[0])])
-            pred_list.extend([y_pred_np[s] for s in range(y_pred_np.shape[0])])
-            input_list.extend([x_np[s] for s in range(x_np.shape[0])])
+            y_true_np = y_true_np[0, ...]
+            x_np = x_np[0, ...]
+            true_list.append(y_true_np)
+            pred_list.append(y_pred_np)
+            input_list.append(x_np)
 
     n = len(input_list)
 

scripts/relevent_mask.py
@@ -9,7 +9,7 @@ edge_size = 256
 step = 10
 
 image_dir = r'data_samples'
-raw_image_dir = 'fixed images'
+raw_image_dir = 'raw_images'
 background_dir = 'background'
 masks_dir = 'masks'
 
@@ -17,6 +17,7 @@ masks_dir = 'masks'
 raw_image_full_path = os.path.join(image_dir, raw_image_dir)
 back_image_full_path = os.path.join(image_dir, background_dir)
 masks_image_full_path = os.path.join(image_dir, masks_dir)
+os.makedirs(masks_image_full_path, exist_ok=True)
 for filename in os.listdir(raw_image_full_path):
     raw_img = np.array(color.rgb2gray(imread(os.path.join(raw_image_full_path, filename))))
     back_img = np.array(imread(os.path.join(back_image_full_path, filename)))

sliding_window.py
@@ -56,9 +56,11 @@ class SlidingWindow:
 
                 weights[:, :, start_x: end_x, start_y: end_y] += torch.ones(
                     size=(image.shape[0], self._network.out_channels, end_x - start_x, end_y - start_y),
-                    dtype=data_type, device=device) * (1 + self.weight_to_add_to_center)
+                    dtype=data_type, device=device)
 
-
+                weights[:, :, x_center_start: x_center_end, y_center_start: y_center_end] += torch.ones(
+                    size=(image.shape[0], self._network.out_channels, x_center_end - x_center_start, y_center_end - y_center_start),
+                    dtype=data_type, device=device) * self.weight_to_add_to_center
             self.progress_event.on_change(int(100 * start_x / h))
         self.progress_event.on_change(100)
 
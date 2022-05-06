0a11c7af81efce8c6df133c9f168721150cb3ec5
Ikalev111 <ikalev111@gmail.com>
2022-05-06 13:13:39 +0300

    Google broke the download so I fixed it.

--- bonemarrow_app.py
+++ bonemarrow_app.py
@@ -28,23 +28,11 @@ def download_file_from_google_drive(file_id, destination):
     google_url = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
-
-    response = session.get(google_url, params={'id': file_id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(google_url, params=params, stream=True)
+    params = {'id': file_id, 'confirm': 't'}
+    response = session.get(google_url, params=params, stream=True)
 
     save_response_content(response, destination)
 
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
 
 
 def save_response_content(response, destination):
